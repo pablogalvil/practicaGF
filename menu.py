868ebdd4605fc1b1ce0cc0f84afec93eb3c58a97
Pablo Galan <pablogalvil53@gmail.com>
2024-11-25 10:36:27 +0100

    Llamamos a las funciones desde menu

--- menu.py
+++ menu.py
@@ -1,3 +1,5 @@
+from operaciones import sumar, restar, multiplicar
+
 def mostrar_menu():
     print("Menú de opciones:")
     print("1- Sumar")
@@ -8,4 +10,19 @@ def mostrar_menu():
     
     # Pedir al usuario que seleccione una opción
     opcion = input("Seleccione una opcion (1-5): ")
+
+    if opcion = 1:
+        num1 = input("Introduce el primer numero: ")
+        num2 = input("Introduce el segundo numero: ")
+        sumar(num1, num2)
+    elif opcion = 2:
+        num1 = input("Introduce el primer numero: ")
+        num2 = input("Introduce el segundo numero: ")
+        restar(num1, num2)
+    elif opcion = 3:
+        num1 = input("Introduce el primer numero: ")
+        num2 = input("Introduce el segundo numero: ")
+        multiplicar(num1, num2)
+    else:
+        print("Opcion no valida")
     return opcion
